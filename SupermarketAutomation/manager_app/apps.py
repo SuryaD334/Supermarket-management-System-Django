5bdc0ed7fab654efd745715ecf47976fe191ff26
Suryadipta Das <lordsurya445@gmail.com>
2024-04-13 19:05:12 +0530

    New changes

--- SupermarketAutomation/manager_app/apps.py
+++ SupermarketAutomation/manager_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainappConfig(AppConfig):
+class ManagerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mainApp'
+    name = 'manager_app'
